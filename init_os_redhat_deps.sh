5928a81a60402c772b76758ab8743c3353275f6e
Iben Rodriguez <Iben@sjultra.com>
2022-03-28 22:20:48 -0700

    updated readme startup script

--- init_os_redhat_deps.sh
+++ init_os_redhat_deps.sh
@@ -1,5 +1,7 @@
-yum update
-yum install -y nmap wget curl
+#! /bin/bash
+set -euo pipefail
+yum clean all
+yum update -y
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
 yum install epel-release
--- init_os_redhat_deps_apache.sh
+++ init_os_redhat_deps_apache.sh
@@ -1,6 +1,8 @@
-yum update
-yum install -y nmap wget curl
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
-yum install epel-release
+yum update -y
+yum install -y epel-release
 yum install -y nmap wget curl iperf3 siege httpd
+service httpd start
+sudo vm_hostname="$(curl -H "Metadata-Flavor:Google" http://169.254.169.254/computeMetadata/v1/instance/name)"
+sudo echo "Page served from: $vm_hostname" | tee /var/www/html/index.html
