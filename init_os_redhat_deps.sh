2b412ee248b15d6ae698069d1d9554ee4185df08
Iben Rodriguez <Iben@sjultra.com>
2022-03-29 18:49:26 -0700

    yum wget

--- init_os_redhat_deps.sh
+++ init_os_redhat_deps.sh
@@ -1,5 +1,4 @@
-wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
+
 yum update -y
-yum install -y epel-releaseyum install epel-release
-yum install -y nmap wget curl iperf3 siege
+yum install -y nmap wget curl iperf3 siege php
+
